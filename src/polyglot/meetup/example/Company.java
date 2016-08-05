bd72c93e7a2d2e884f5e89d2c22b451156a18c49
Pushkar N Kulkarni <pushkar.rvce@gmail.com>
2016-08-06 01:56:11 +0530

    Add diamond operator

--- src/polyglot/meetup/example/Company.java
+++ src/polyglot/meetup/example/Company.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 
 public class Company {
 	
-	ArrayList<Employee> employees = new ArrayList<Employee>();
+	ArrayList<Employee> employees = new ArrayList<>();
 	
 	public void addEmployee(Employee e) {
 		employees.add(e);
@@ -19,7 +19,7 @@ public class Company {
 	
 	/* Employee-bonus map*/
 	public HashMap<Employee,Double> getEmployeeBonuses() {
-		HashMap<Employee,Double> bonuses = new HashMap<Employee,Double>();
+		HashMap<Employee,Double> bonuses = new HashMap<>();
 		for (Employee e :  employees ) {
 			double bonus = e.getBonus(bonusCalc);
 			bonuses.put(e, bonus);
