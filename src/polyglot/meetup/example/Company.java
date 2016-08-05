ca0f53cc412b96c78f02e16b3bee585f0e3d44ca
Pushkar N Kulkarni <pushkar.rvce@gmail.com>
2016-08-06 02:08:08 +0530

    Replaced anonymous class by lambda

--- src/polyglot/meetup/example/Company.java
+++ src/polyglot/meetup/example/Company.java
@@ -10,18 +10,12 @@ public class Company {
 	public void addEmployee(Employee e) {
 		employees.add(e);
 	}
-
-	BonusCalculator bonusCalc = new BonusCalculator() {
-		public double calculate(double salary, int rating) {
-			return salary * 0.2 * rating;
-		}
-	};
 	
 	/* Employee-bonus map*/
 	public HashMap<Employee,Double> getEmployeeBonuses() {
 		HashMap<Employee,Double> bonuses = new HashMap<>();
 		for (Employee e :  employees ) {
-			double bonus = e.getBonus(bonusCalc);
+			double bonus = e.getBonus((salary, rating) ->salary * 0.2 * rating );
 			bonuses.put(e, bonus);
 		}
 		return bonuses;
